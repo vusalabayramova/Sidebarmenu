974c395e71997e743f746182674935128be84c77
Vüsalə <70525218+vusalabayramova@users.noreply.github.com>
2021-10-02 18:50:14 +0400

    Add files via upload

---
+++ sidebarmenu/sidebarmenu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+$('#open').click(function(){
+    $(this).toggleClass('fa-times');
+$('.sidebar').toggleClass('togglebar');  
+$('.menu').toggleClass('togglemenu');  
+})    
+})
